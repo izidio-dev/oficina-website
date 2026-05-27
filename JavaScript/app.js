130819444418b9d3edbffee8f09f432d3b939b3e
izidio-dev <brmizidio@gmail.com>
2026-05-27 13:40:56 -0300

    Projeto finalizado, feito com HTML, CSS e JS

---
+++ JavaScript/app.js
@@ -0,0 +1,6 @@
+// Arquivo JavaScript para Oficina Piloto
+// Adicione suas funcionalidades aqui!
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Website Oficina Piloto carregado com sucesso!');
+});
